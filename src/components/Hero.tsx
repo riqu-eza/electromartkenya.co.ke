0b24bcfdc3f186487acf324953e101954e944523
kang`ethe <offline@kali.dkmuthunga.com>
2025-08-08 14:13:10 +0300

    ready to commit

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,11 +10,11 @@ interface IProps {
 export default function Hero({setRatio} : IProps) {
   return (
     <div className='w-full h-screen relative'>
-      <Image src={'/intro.jpg'} alt='no Image' fill className="w-full h-full object-fill hidden md:block " onLoadingComplete={({ naturalWidth, naturalHeight }) =>
+      <Image src={'/intro.png'} alt='no Image' fill className="w-full h-full object-fill hidden md:block " onLoadingComplete={({ naturalWidth, naturalHeight }) =>
           setRatio(naturalWidth / naturalHeight)
         } />
       <div className='w-full flex-col md:hidden h-full relative flex items-center px-3 justify-center text-center'>
-        <Image src={'/mob-intro.jpg'} alt='no image'  fill />
+        <Image src={'/mob-intro.png'} alt='no image'  fill />
 
         <h1 className='mb-2 text-xl text-white/90 z-10 font-semibold '>Work hard, lift harder - Get fit, feel fitter with our gear!</h1>
         <Link href={"/#my-Categories"} className='btn btn-ghost border border-orange-600 text-white/90 hover:bg-orange-600  z-40'>Shop Now</Link>
